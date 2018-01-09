962f13c9eaf26b906fba8c2edd7d440676242094
[mahmoudabulebdeh] <[m.a.uae@live.com]>
2018-01-09 23:16:39 +0000

    Test: generateMultiplicationMatrix() is defined
    Adding a failing test: The generateMultiplicationMatrix() method should
    be defined.

---
+++ js/spec/multiplicationTable.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Mahmoud on 09/01/2018.
+ */
+
+
+describe("multiplication matrix generator",function () {
+    it("should be defined", function () {
+        expect(generateMultiplicationMatrix()).toBeDefined;
+    });
+});
+
