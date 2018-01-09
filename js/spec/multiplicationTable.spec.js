2e02618384a32226a9cc83a8f08213b992bef8d3
[mahmoudabulebdeh] <[m.a.uae@live.com]>
2018-01-09 23:21:50 +0000

    Code to pass [Test: generateMultiplicationMatrix() is defined]
    Adding the definition of the generateMultiplicationMatrix() method to
    pass the defined test.

--- js/spec/multiplicationTable.spec.js
+++ js/spec/multiplicationTable.spec.js
@@ -3,6 +3,15 @@
  */
 
 
+/**
+ * Method that takes an input Array of the size n
+ * @param arr is an Array of the size n
+ */
+function generateMultiplicationMatrix(arr){
+
+
+}
+
 describe("multiplication matrix generator",function () {
     it("should be defined", function () {
         expect(generateMultiplicationMatrix()).toBeDefined;
