9d63181fb7f2831bd46a826af3fd24094b83fac1
[mahmoudabulebdeh] <[m.a.uae@live.com]>
2018-01-09 18:09:57 +0000

    Add the tested method validateInput(x) to the production code
    Add the tested method validateInput(x) to the production code and use
    it to validate user inputs. the method is added to the app object
    js/app/index.js and utilised in the same file to check user inputs.

--- js/app/index.js
+++ js/app/index.js
@@ -1,3 +1,41 @@
 /**
  * Created by Mahmoud on 09/01/2018.
  */
+
+// function to be called when clicking on the submit button
+document.getElementById("submit").addEventListener("click", function(){
+
+    // read user input
+    input = document.getElementById("numb").value;
+
+    // validate user input
+    var validation = app.validateInput(input);
+
+    // check if the input is valid or invalid and return the respective message
+    if (validation==-1){
+        var message = "'"+ input +"' is not a valid input! Please Enter an Integer number greater or equal to one.";
+        alert(message);
+    } else {
+        alert("valid input!");
+    }
+});
+
+// main functionality
+var app = {
+
+    /**
+     * Method to validate user inputs
+     * @param x is the input value
+     * @returns the input value if it was an integer value >= 1 (valid) otherwise it returns -1 (invalid)
+     */
+    validateInput: function(x) {
+        // if x isn't a number or not an Integer value or less than 1
+        if (isNaN(x) || x % 1 !== 0 || x <1){
+            return -1;
+        }
+        // if x is a valid Integer number
+        else{
+            return x;
+        }
+    }
+}
