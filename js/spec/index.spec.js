6cb18a5c35ccbae63826c0b990538b948a37423d
[mahmoudabulebdeh] <[m.a.uae@live.com]>
2018-01-09 13:42:29 +0000

    Creating UI - updating index.html, css/styles.css
    creating the user-interface, a form to take user input and display
    generated primes mulitiplication table

--- js/spec/index.spec.js
+++ js/spec/index.spec.js
@@ -1,3 +1,8 @@
 /**
  * Created by Mahmoud on 09/01/2018.
  */
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
