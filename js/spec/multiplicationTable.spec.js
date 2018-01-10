4b775ec16180d41f0075053a0aef7dcf5687bb58
[mahmoudabulebdeh] <[m.a.uae@live.com]>
2018-01-10 07:35:47 +0000

    Test: generateMultiplicationMatrix(arr) takes an Array of the size n and returns an Array of the size [n+1][n+1]
    Adding a failing test: generateMultiplicationMatrix(arr) takes an Array
    of the size n and returns a two dimensional Array of the size [n+1][n+1]

--- js/spec/multiplicationTable.spec.js
+++ js/spec/multiplicationTable.spec.js
@@ -16,5 +16,32 @@ describe("multiplication matrix generator",function () {
     it("should be defined", function () {
         expect(generateMultiplicationMatrix()).toBeDefined;
     });
+
+    it("takes an Array of the size n and returns a two dimensional Array of the size [n+1][n+1]", function () {
+        var arr1 = [1,2];
+        var n = arr1.length;
+        var m = generateMultiplicationMatrix(arr1);
+        var l = m.length;
+        var w = m[0].length;
+        expect(l).toEqual(w);
+        expect(l).toEqual(n+1);
+
+        var arr2 = [1,2,3,4,5,6,7,8,9,10];
+        var n = arr2.length;
+        var m = generateMultiplicationMatrix(arr2);
+        var l = m.length;
+        var w = m[0].length;
+        expect(l).toEqual(w);
+        expect(l).toEqual(n+1);
+
+        var arr3 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+        var n = arr2.length;
+        var m = generateMultiplicationMatrix(arr2);
+        var l = m.length;
+        var w = m[0].length;
+        expect(l).toEqual(w);
+        expect(l).toEqual(n+1);
+
+    });
 });
 
