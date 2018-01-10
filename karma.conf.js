372ecfdd0ae2c1693cb777bec06bacf709e4a58b
[mahmoudabulebdeh] <[m.a.uae@live.com]>
2018-01-10 17:35:41 +0000

    update test configuration to test the integration on Chrome, Safari, Firefox
    Install the test coverage plugin and update the test configuration to
    test  the code integration in different browsers (Chrome, Safari,
    Firefox)

--- karma.conf.js
+++ karma.conf.js
@@ -54,8 +54,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['Chrome','Safari','Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
